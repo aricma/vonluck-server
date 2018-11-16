40633a73c30439d82240b83aac9702f62f5bcf89
AdrianMindak <adrian.richard.mindak@gmail.com>
2018-11-16 19:58:11 +0100

    add env var PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const nodemailer = require('nodemailer')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 // let selfSignedConfig = {
     // host: 'host129.checkdomain.com',
     // port: 587,
