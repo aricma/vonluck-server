7fa07db6053f02f2611cead253ffd97bedee9710
AdrianMindak <adrian.richard.mindak@gmail.com>
2018-11-19 18:25:15 +0100

    add * to CORS

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // CORS for von-luck.de
 app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "von-luck.de", "2003:dd:7f25:5200:e51e:f80c:19ef:22fd");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
  });
