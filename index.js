7d7c7c3226d3eaaa4707cf8df13fd49caf20c5f5
AdrianMindak <adrian.richard.mindak@gmail.com>
2018-11-19 23:34:03 +0100

    set CORS to something else as a test

--- index.js
+++ index.js
@@ -28,13 +28,12 @@ let transporter = nodemailer.createTransport(transporterConfiguration)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// CORS for von-luck.de
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
- });
+});
 
 // MIDDLEWARE
 app.use((req, res, next) => {
