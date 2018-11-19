0910bde795f7e6f40379d0f2c111513b5d6735ae
AdrianMindak <adrian.richard.mindak@gmail.com>
2018-11-19 22:52:58 +0100

    add Content-Type to CORS

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // CORS for von-luck.de
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   next();
  });
 
